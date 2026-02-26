9dcb321628cef0e5d2f0850a7398c448a6fda5cc
Reedsaltty <leaphourleumac@gmail.com>
2026-02-26 14:25:34 +0700

    commmit

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,8 @@
-function Header() {
+function Header({logo}) {
   return (
     <header className="header" id="header">
-     
+        <img src={logo} alt="Logo" />
+        <h1>Students results for (CS-Bush)</h1>
     </header>
   );
 }
