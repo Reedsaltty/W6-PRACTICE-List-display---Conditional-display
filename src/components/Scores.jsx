dbeec0f87b1753a4b6edafca7c5a401c5d8d4d3b
Reedsaltty <leaphourleumac@gmail.com>
2026-02-26 21:53:39 +0700

    ./src/components/Scores.jsx

--- src/components/Scores.jsx
+++ src/components/Scores.jsx
@@ -1,34 +1,33 @@
-
+import Stat from "./Stat"
 export default function Scores({Course_Name, results}) {
     return (
-        <main className="scores-container">
+        
         <div className="scores">
           <h1>{Course_Name}</h1>
 
-          <table>
-            <thead>
-              <tr>
-                <th>First name</th>
-                <th>Last name</th>
-                <th>Score</th>
-              </tr>
-            </thead>
-            <tbody>
-                {results.map((element,index) => {
-                     return (
-                        <tr key={index}>
-                            <td>{element.firstName}</td>
-                            <td>{element.lastName}</td>
-                            <td className={element.score < 50 ? 'warning' : ''} >{element.score}</td>
-                        </tr>  
-                     )
-                })}
-               
-              
-            </tbody>
-          </table>
+            <table>
+              <thead>
+                <tr>
+                  <th>First name</th>
+                  <th>Last name</th>
+                  <th>Score</th>
+                </tr>
+              </thead>
+              <tbody>
+                  {results.map((element,index) => {
+                       return (
+                          <tr key={index}>
+                              <td>{element.firstName}</td>
+                              <td>{element.lastName}</td>
+                              <td className={element.score < 50 ? 'warning' : ''} >{element.score}</td>
+                          </tr>  
+                       )
+                  })}
+              </tbody>
+            </table>
+            <Stat Score={results}/>
         </div>
-      </main>
+
 
     )
 }
